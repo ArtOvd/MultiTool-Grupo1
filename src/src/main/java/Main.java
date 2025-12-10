b333b7d4054d5d7c58d290ade5a5edf585fd0bf4
Artem Ovdiienko <artovd.alu.edu.gva.es>
2025-12-10 13:52:41 +0100

    Menú principal hecho. Sin funciones útiles, solo estructura.

--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
-    static void main() {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("1 - Gestor de números.");
+            System.out.println("2 - Conversor multitipo.");
+            System.out.println("3 - Analizador de texto.");
+            System.out.println("4 - Gestión de tareas.");
+            System.out.println("5 - Salir");
+            System.out.print("Tu opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
 
-
-
+            }
+        } while (opcion != 0);
     }
 
 }
