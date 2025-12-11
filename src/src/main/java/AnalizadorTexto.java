cf6d464d2914e9f7b00f06eb4f79a6ae10b8d18c
Vladyslav <vladum@alu.edu.gva.es>
2025-12-11 14:08:03 +0100

    Primer commit (Menu de programa): He creado el metodo que muestre el menu del programa. He creado otro metodo que pide elegir opcion del menu al usuario. Testeo: He ejecutado el codigo y estaba introduciendo cada opcion y miraba si funcióna de forma correcta.

--- src/src/main/java/AnalizadorTexto.java
+++ src/src/main/java/AnalizadorTexto.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class AnalizadorTexto {
+    static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int opcion;
+        do {
+            MenuAnalizadorTexto();
+            opcion = ElegirOpcion(sc);
+        }while (opcion != 0 );
+    }
+
+
+    static void MenuAnalizadorTexto() {
+        System.out.println("======================================");
+        System.out.println("      MENU / ANALIZADOR DE TEXTO      ");
+        System.out.println("======================================");
+        System.out.println();
+        System.out.println("1. Contar vocales");
+        System.out.println("2. Contar palabras");
+        System.out.println("3. Palabra más larga");
+        System.out.println("0. Volver");
+    }
+
+    static int ElegirOpcion(Scanner sc) {
+        System.out.println();
+        System.out.print("Elige opción: ");
+        return sc.nextInt();
+    }
+
+
+
 }
