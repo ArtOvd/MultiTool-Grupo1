62ab1085b16aa795644356ee738caa36620d4fa4
artem <artovd@alu.edu.gva.es>
2025-12-13 18:37:00 +0100

    Metodo para añadir tarea con validación creado.

--- src/src/main/java/GestionTareas.java
+++ src/src/main/java/GestionTareas.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class GestionTareas {
     static Scanner sc = new Scanner(System.in);
     static String[] tareas = new String[15];
-
+    static int index = 0;
     public static void main(String[] args) {
-
 
     }
 
@@ -16,5 +15,26 @@ public class GestionTareas {
         System.out.println("0. Volver");
     }
 
+    static void addTarea() {
+        boolean valido = false;
+        String tarea = "";
+        do {
+            tarea = "";
+            System.out.print("Indica el nombre de la tarea: ");
+            tarea = sc.nextLine().trim().toLowerCase();
+            if (tarea.isEmpty()) {
+                System.out.println("No se puede añadir una tarea sin nombre.");
+                break;
+            } else if (tarea.length() < 10) {
+                System.out.println("El nombre de la tarea es muy corto.");
+                break;
+            }
+            valido = true;
+        } while (false);
+            if (valido) {
+                tareas[index] = tarea;
+                System.out.println("Tarea añadida correctamente.");
+            }
+    }
 
 }
