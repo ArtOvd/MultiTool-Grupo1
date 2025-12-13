2fb8426aeb757f24796fb6b1d7ad7e326ffa4f16
artem <artovd@alu.edu.gva.es>
2025-12-13 18:14:23 +0100

    Variables globales y meñu principal creados.

--- src/src/main/java/GestionTareas.java
+++ src/src/main/java/GestionTareas.java
@@ -1,7 +1,20 @@
-public class GestionTareas {
+import java.util.Scanner;
 
-    public static void mostrarGestorTareas() {
+public class GestionTareas {
+    static Scanner sc = new Scanner(System.in);
+    static String[] tareas = new String[15];
+
+    public static void main(String[] args) {
+
 
     }
+
+    static void imprimirMenu() {
+        System.out.println("1. Añadir tarea");
+        System.out.println("2. Listar tareas");
+        System.out.println("3. Buscar palabra");
+        System.out.println("0. Volver");
+    }
+
 
 }
