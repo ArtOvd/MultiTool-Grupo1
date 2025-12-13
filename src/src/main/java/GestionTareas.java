468156bbacde8267fd7fc91802c90bce5fd24d5a
artem <artovd@alu.edu.gva.es>
2025-12-13 19:11:59 +0100

    Metodo para buscar tareas creado. Correción de validación en metodo addTarea

--- src/src/main/java/GestionTareas.java
+++ src/src/main/java/GestionTareas.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class GestionTareas {
     static Scanner sc = new Scanner(System.in);
     static String[] tareas = new String[15];
-    static int index = 0;
+    static int index = 3;
     public static void main(String[] args) {
 
     }
@@ -26,8 +26,14 @@ public class GestionTareas {
                 System.out.println("No se puede añadir una tarea sin nombre.");
                 break;
             } else if (tarea.length() < 10) {
-                System.out.println("El nombre de la tarea es muy corto.");
+                System.out.println("El nombre de la tarea tiene que contener más de 10 caracteres.");
                 break;
+            }
+            for (int i = 0; i < index; i++) {
+                if (tareas[i].equals(tarea)) {
+                    System.out.println("Esta tarea ya existe");
+                    break;
+                }
             }
             valido = true;
         } while (false);
@@ -41,9 +47,37 @@ public class GestionTareas {
         System.out.println("=================");
         System.out.println("LISTA DE TAREAS");
         System.out.println("=================");
-        for (int i = 0; i <= index; i++) {
+        for (int i = 0; i < index; i++) {
             System.out.println("Tarea " + (i + 1) + ": " +  tareas[i]);
         }
+    }
+
+    static void buscarTareas() {
+        boolean encontrado = false;
+        System.out.println("====================");
+        System.out.println("BÚSQUEDA DE TAREAS");
+        System.out.println("====================");
+        System.out.print("Indica el nombre de la tarea: ");
+        String entrada = sc.nextLine().trim().toLowerCase();
+        if (entrada.isEmpty()) {
+            System.out.println("No se admite entrada vacía.");
+        } else {
+            System.out.println("Tareas encontradas:");
+            for (int i = 0; i < index; i++) {
+                if (tareas[i].equals(entrada)) {
+                    encontrado = true;
+                    System.out.println(tareas[i]);
+                    break;
+                } else if (tareas[i].startsWith(entrada) || tareas[i].contains(entrada)) {
+                    encontrado = true;
+                    System.out.println(tareas[i]);
+                }
+            }
+            if (!encontrado) {
+                System.out.println("No hay tareas con este nombre.");
+            }
+        }
+
     }
 
 }
