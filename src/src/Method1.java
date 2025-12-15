24a009346212f4e3ed3485e8d27f8180953f8122
wilthomur@alu.edu.gva.es <wilthomur@alu.edu.gva.es>
2025-12-15 11:46:51 +0100

    Creado fichero clase para el método 1, junto con el método y un par de variables para el menú.

---
+++ src/src/Method1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Method1 {
+
+    public static Scanner sc (){
+        return new Scanner(System.in);
+    }
+
+    public static void method1(){
+
+        int subMenu = sc().nextInt();
+        int[] arrayNums = new int[20];
+
+    }
+}
