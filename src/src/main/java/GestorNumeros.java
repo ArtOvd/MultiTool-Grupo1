e9e62d592cfc5a5ffcb65a6d669a57f3129e2fcf
wilthomur@alu.edu.gva.es <wilthomur@alu.edu.gva.es>
2025-12-16 09:39:59 +0100

    Creado fichero para la clase "Gestor de Números", con el esqueleto para el método y las variables

--- src/src/main/java/GestorNumeros.java
+++ src/src/main/java/GestorNumeros.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class GestorNumeros {
+
+    public static Scanner sc (){
+        return new Scanner(System.in);
+    }
+
+    public static void gestorNumerosMenu(){
+        int[] arrayNums = new int[20];
+        int subMenu = 0;
+    }
 }
