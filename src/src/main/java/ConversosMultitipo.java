94f67fbe4002d1d06d57965e40bed1e1418da6bf
AdrianSubies <adriansg220107@gmail.com>
2025-12-16 10:31:05 +0100

    He completado metodos a excepcion de el ultimo que es el que evita que se utilizen otro numeros y pida un dato tipo INT

--- src/src/main/java/ConversosMultitipo.java
+++ src/src/main/java/ConversosMultitipo.java
@@ -1,9 +1,11 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ConversosMultitipo {
     static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+    conversormultitipo();
     }
     //metodo con el switch el cual es el menu/seleccionador de la aplicacion
 
@@ -39,23 +41,46 @@ public class ConversosMultitipo {
 
     }
     static int MostrarMenuYleeropcion(){
+        int opcion;
 
+        while (true){
+            System.out.println( ">\n CONVERSOR DE DATOS");
+            System.out.println("> 1. Celsius a Farenheit");
+            System.out.println("> 2. Metros a Pies");
+            System.out.println("> 3. Kg a Libras");
+            System.out.println("> 0. Salir");
+            System.out.println("----->>>> elige una opción: ");
+
+            if(sc.hasNextInt()){
+                opcion = sc.nextInt();
+                sc.nextLine();
+                return opcion;
+            }else{
+                System.out.println(">>>> Error: solo se aceptan números <<<<");
+            }
+        }
     }
 
     static void conversorCelsiusFarenheit(){
-
-    }
-
-    static void conversorKgLibras(){
-
+        double celsius = leerDoubleSeguro("-> Introduce grados Celsius: ");
+        double farenheit = (celsius * 9 / 5) +32;
+        System.out.println(celsius + " C = " +farenheit + " F");
     }
 
     static void conversorMetrosPies(){
+        double metros = leerDoubleSeguro("-> introduce metros: ");
+        double pies = metros * 3.28084;
+        System.out.println(metros + " metros = " + pies + " pies");
+    }
 
+    static void conversorKgLibras(){
+        double kg = leerDoubleSeguro("-> Introduce kilos: ");
+        double libras = kg * 2.20462;
+        System.out.println(kg + "kg = " + libras +  " libras");
     }
 
     //metodo para que solo acepte numeros
     static double leerDoubleSeguro(String mensaje) {
-
+return 0;
     }
 }
