68664ce7c5148b87cf4174a9ef53898da55e44f6
AdrianSubies <adriansg220107@gmail.com>
2025-12-16 13:14:28 +0100

    He movido el fichero de mi utilidad de mi rama a la rama dev

--- src/src/main/java/ConversosMultitipo.java
+++ src/src/main/java/ConversosMultitipo.java
@@ -1,2 +1,99 @@
+
+import java.util.Scanner;
+
 public class ConversosMultitipo {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    conversormultitipo();
+    }
+
+    //metodo con el switch el cual es el menu/seleccionador de la aplicacion
+    public static void conversormultitipo(){
+
+        int opcion;
+        do{
+            opcion = MostrarMenuYleeropcion();
+            switch (opcion){
+                case 1:
+                    conversorCelsiusFarenheit();
+                    break;
+                case 2:
+                    conversorMetrosPies();
+                    break;
+                case 3:
+                    conversorKgLibras();
+                    break;
+                case 0:
+                    System.out.println("---->>>  Saliedo...");
+                    break;
+                default:
+                    System.out.println("---- Opcion no válida ----");
+            }
+
+            if (opcion != 0){
+                System.out.println("\n-----Pulsa ENTER para continuar-----");
+                sc.nextLine();
+            }
+
+        }while(opcion !=0);
+
+
+    }
+
+    static int MostrarMenuYleeropcion(){
+        int opcion;
+
+        while (true){
+            System.out.println( ">\n CONVERSOR DE DATOS");
+            System.out.println("> \n1. Celsius a Farenheit");
+            System.out.println("> 2. Metros a Pies");
+            System.out.println("> 3. Kg a Libras");
+            System.out.println("> 0. Salir");
+            System.out.println("----->>>> elige una opción: ");
+
+            if(sc.hasNextInt()){
+                opcion = sc.nextInt();
+                sc.nextLine(); //clean buffer
+                return opcion;
+            }else{
+                System.out.println(">>>> Error: solo se aceptan números <<<<");
+            }
+        }
+    }
+
+    static void conversorCelsiusFarenheit(){
+        double celsius = leerDoubleSeguro("-> Introduce grados Celsius: ");
+        double farenheit = (celsius * 9 / 5) +32;
+        System.out.println(celsius + " C = " +farenheit + " F");
+    }
+
+    static void conversorMetrosPies(){
+        double metros = leerDoubleSeguro("-> introduce metros: ");
+        double pies = metros * 3.28084;
+        System.out.println(metros + " metros = " + pies + " pies");
+    }
+
+    static void conversorKgLibras(){
+        double kg = leerDoubleSeguro("-> Introduce kilos: ");
+        double libras = kg * 2.20462;
+        System.out.println(kg + "kg = " + libras +  " libras");
+    }
+
+    //metodo para que solo acepte numeros (COMPLETADO)
+    static double leerDoubleSeguro(String mensaje) {
+        double numero;
+
+        while (true){
+            System.out.println(mensaje);
+        if(sc.hasNextDouble()){
+            numero = sc.nextDouble();
+            sc.nextLine();
+            return numero;
+        }else{
+            System.out.println( "Error solo se permiten numeros");
+            sc.nextLine();
+            }
+        }
+    }
 }
